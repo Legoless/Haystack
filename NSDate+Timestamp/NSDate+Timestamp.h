73e7a03de172d5eda9dff4019776f86e8939b0ce
Dal Rupnik <legoless@gmail.com>
2014-01-08 18:45:07 +0100

    Added implementation of NSDate category.
    Implemented methods of NSDate+Timestamp category.

--- NSDate+Timestamp/NSDate+Timestamp.h
+++ NSDate+Timestamp/NSDate+Timestamp.h
@@ -7,22 +7,26 @@
 @interface NSDate (Timestamp)
 
 /*!
- *  Transforms NSDate into Unix timestamp in milliseconds
- *  @param date to be transformed to Unix timestamp
- *  @return unixTimestamp of passed date
+ *  Converts NSDate into Unix timestamp in milliseconds.
+ *
+ *  @param NSDate date to be converted to Unix timestamp
+ *  @return NSTimeInterval Unix timestamp of passed date
  */
-+ (NSTimeInterval)getUnixTimeStampFromNSDate:(NSDate *)date;
++ (NSTimeInterval)unixTimestampFromDate:(NSDate *)date;
 
 /*!
- * @return time interval since/until the Unix timestamp in milliseconds.
+ * Returns time interval since/until the Unix timestamp in milliseconds.
  *
  * If timestamp is in the future, interval is positive, if it is in the past
  * it is negative.
+ *
+ * @param NSTimeInterval Unix timestamp
+ * @return NSTimeInterval Time interval in milliseconds
  */
-+ (NSTimeInterval)timeIntervalUntilUnixTimeStamp:(NSNumber *)timestamp;
++ (NSTimeInterval)timeIntervalUntilUnixTimeStamp:(NSTimeInterval)timestamp;
 
 /*!
- * Returns Unix timestamp of current date
+ * Returns Unix timestamp of current date object.
  *
  * @return NSTimeInterval Unix timestamp
  */
--- NSDate+Timestamp/NSDate+Timestamp.m
+++ NSDate+Timestamp/NSDate+Timestamp.m
@@ -6,4 +6,22 @@
 
 @implementation NSDate (Timestamp)
 
++ (NSTimeInterval)unixTimestampFromDate:(NSDate *)date
+{
+    NSTimeInterval timestamp = ([date timeIntervalSince1970] * 1000.0);
+    
+    return timestamp;
+}
+
++ (NSTimeInterval)timeIntervalUntilUnixTimeStamp:(NSTimeInterval)timestamp;
+{
+    NSDate *timeStampDate = [NSDate dateWithTimeIntervalSince1970:([timestamp doubleValue] / 1000.0)];
+    
+    return [timeStampDate timeIntervalSinceNow] * 1000.0;
+}
+
+- (NSTimeInterval)unixTimestamp
+{
+    return [date timeIntervalSince1970] * 1000.0;
+}
 @end
