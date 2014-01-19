7059dd5ff0468ce62202cde074c0372041d0bf7a
Dal Rupnik <legoless@gmail.com>
2014-01-19 17:26:54 +0100

    Finished documentation with Hex colors.

--- UIColor+Flat/UIColor+Flat.h
+++ UIColor+Flat/UIColor+Flat.h
@@ -19,37 +19,88 @@
  */
 + (UIColor *)emeraldColor;
 
+/*!
+ * Nephritis color: #27ae60
+ */
 + (UIColor *)nephritisColor;
 
+/*!
+ * Peter River color: #3498db
+ */
 + (UIColor *)peterRiverColor;
 
+/*!
+ * Belize Hole color: #2980b9
+ */
 + (UIColor *)belizeHoleColor;
 
+/*!
+ * Amethyst color: #9b59b6
+ */
 + (UIColor *)amethystColor;
 
+/*!
+ * Wisteria color: #8e44ad
+ */
 + (UIColor *)wisteriaColor;
 
+/*!
+ * Wet Asphalt color: #34495e
+ */
 + (UIColor *)wetAsphaltColor;
 
+/*!
+ * Midnight Blue color: #2c3e50
+ */
 + (UIColor *)midnightBlueColor;
 
+/*!
+ * Sun Flower color: #f1c40f
+ */
 + (UIColor *)sunFlowerColor;
 
+/*!
+ * Carrot color: #e67e22
+ */
 + (UIColor *)carrotColor;
 
+/*!
+ * Orange Fruit color: #f39c12
+ */
 + (UIColor *)orangeFruitColor;
 
+/*!
+ * Pumpkin color: #d35400
+ */
 + (UIColor *)pumpkinColor;
 
+/*!
+ * Alizarin color: #e74c3c
+ */
 + (UIColor *)alizarinColor;
 
+/*!
+ * Pomegranate color: #c0392b
+ */
 + (UIColor *)pomegranateColor;
 
+/*!
+ * Clouds color: #ecf0f1
+ */
 + (UIColor *)cloudsColor;
 
+/*!
+ * Silver color: #bdc3c7
+ */
 + (UIColor *)silverColor;
 
+/*!
+ * Concrete color: #95a5a6
+ */
 + (UIColor *)concreteColor;
 
+/*!
+ * Asbestos color: #7f8c8d
+ */
 + (UIColor *)asbestosColor;
 @end
