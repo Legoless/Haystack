1a76f8fcd659edd46db386fd2db7df56d77ccecc
Dal Rupnik <legoless@gmail.com>
2014-01-25 19:00:46 +0100

    Added UIButton+Position category.
    Added category positions the image of the button below, above, left or
    right of the title label.

---
+++ UIButton+Position/UIButton+Position.m
@@ -0,0 +1,25 @@
+//
+//  UIButton+Position.m
+//
+
+@implementation UIButton (Position)
+
+- (void)setTitleBelowWithSpacing:(CGFloat)spacing
+{
+    CGFloat spacing = spacing;
+    
+    // Get the size of the elements here for readability
+    CGSize imageSize = self.imageView.frame.size;
+    CGSize titleSize = self.titleLabel.frame.size;
+    
+    // Get the height they will take up as a unit
+    CGFloat totalHeight = (imageSize.height + titleSize.height + spacing);
+    
+    // raise the image and push it right to center it
+    self.imageEdgeInsets = UIEdgeInsetsMake(- (totalHeight - imageSize.height), 0.0, 0.0, - titleSize.width);
+    
+    // lower the text and push it left to center it
+    self.titleEdgeInsets = UIEdgeInsetsMake(0.0, - imageSize.width, - (totalHeight - titleSize.height), 0.0);
+}
+
+@end
