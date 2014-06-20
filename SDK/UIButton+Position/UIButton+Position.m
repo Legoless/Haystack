0f0403f56c097b602dab0c99e15ef10d4d7fbe72
Dal Rupnik <legoless@gmail.com>
2014-06-20 12:35:55 +0200

    Update UIButton+Position.m

--- SDK/UIButton+Position/UIButton+Position.m
+++ SDK/UIButton+Position/UIButton+Position.m
@@ -7,16 +7,20 @@
 - (void)setTitleBelowWithSpacing:(CGFloat)spacing
 {
     // Get the size of the elements here for readability
+
     CGSize imageSize = self.imageView.frame.size;
     CGSize titleSize = self.titleLabel.frame.size;
-    
+
     // Get the height they will take up as a unit
+
     CGFloat totalHeight = (imageSize.height + titleSize.height + spacing);
-    
-    // raise the image and push it right to center it
-    self.imageEdgeInsets = UIEdgeInsetsMake(- (totalHeight - imageSize.height), 0.0, 0.0, - titleSize.width);
-    
+
+    // Raise the image and push it right to center it
+
+    self.imageEdgeInsets = UIEdgeInsetsMake(-(totalHeight - imageSize.height), self.imageEdgeInsets.left, 0.0, - titleSize.width);
+
     // lower the text and push it left to center it
+
     self.titleEdgeInsets = UIEdgeInsetsMake(0.0, - imageSize.width, - (totalHeight - titleSize.height), 0.0);
 }
 
