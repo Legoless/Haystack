b237d7b787bba58ac6d87704f76128f174a1dea8
Dal Rupnik <legoless@gmail.com>
2014-07-04 13:03:47 +0200

    Fixed centering position of image in button below

--- SDK/UIButton+Position/UIButton+Position.m
+++ SDK/UIButton+Position/UIButton+Position.m
@@ -7,20 +7,23 @@
 - (void)setTitleBelowWithSpacing:(CGFloat)spacing
 {
     // Get the size of the elements here for readability
-
     CGSize imageSize = self.imageView.frame.size;
     CGSize titleSize = self.titleLabel.frame.size;
+    CGSize textSize = [self.titleLabel.text sizeWithAttributes:@{ NSFontAttributeName : self.titleLabel.font }];
+    CGSize frameSize = self.frame.size;
 
     // Get the height they will take up as a unit
-
     CGFloat totalHeight = (imageSize.height + titleSize.height + spacing);
 
-    // Raise the image and push it right to center it
+    //
+    // Image should be in center, so we need to move it if it is not
+    //
+    
+    CGFloat leftInset = fabs(frameSize.width - imageSize.width) / 2.0;
+    
+    self.imageEdgeInsets = UIEdgeInsetsMake(-(totalHeight - imageSize.height), leftInset, 0.0, 0.0);
 
-    self.imageEdgeInsets = UIEdgeInsetsMake(-(totalHeight - imageSize.height), self.imageEdgeInsets.left, 0.0, - titleSize.width);
-
-    // lower the text and push it left to center it
-
+    // Lower the text and push it left to center it
     self.titleEdgeInsets = UIEdgeInsetsMake(0.0, - imageSize.width, - (totalHeight - titleSize.height), 0.0);
 }
 
