28fc08fb3fed09821f2fe3a28fff77d0393d5bec
Dal Rupnik <legoless@gmail.com>
2014-07-16 09:29:53 +0200

    Added Xcode Beta SDK copy script

---
+++ Scripts/sdk.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#
+# Find Xcode 6 Beta
+#
+
+SDK_DIR='/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs'
+
+echo '[COPY]: Searching for Xcode app...'
+
+XCODE_STABLE=$(find / -name Xcode.app -type d -maxdepth 2)
+
+echo '[COPY]: Stable Xcode found.'
+echo '[COPY]: Searching for Xcode6 app...'
+
+XCODE_BETA=$(find / -name Xcode6*.app -type d -maxdepth 2)
+
+echo '[COPY]: Beta Xcode6 found.'
+
+#
+# Search for stable SDK in stable directory
+#
+
+XCODE_STABLE_SDK=$XCODE_STABLE$SDK_DIR
+XCODE_BETA_TARGET=$XCODE_BETA$SDK_DIR
+
+echo '[COPY]: Searching for stable SDK...'
+
+SDKS=$(find $XCODE_STABLE_SDK -type d -name iPhoneOS*.sdk)
+
+for FOUND_SDK in $SDKS
+do
+    echo '[COPY]: Copying' $FOUND_SDK ' to Xcode Beta...'
+
+    cp -rf $FOUND_SDK $XCODE_BETA_TARGET
+done
