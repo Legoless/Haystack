c821234974f9fd163a38863853057a7ad7795521
Dal Rupnik <legoless@gmail.com>
2015-01-05 14:49:19 +0100

    Added string count method

--- SDK/Categories/NSString+Additional/NSString+Additional.m
+++ SDK/Categories/NSString+Additional/NSString+Additional.m
@@ -16,4 +16,23 @@
     return [self hasPrefix:string];
 }
 
+- (NSUInteger)numberOfOccurencesOfString:(NSString *)string
+{
+    NSUInteger count = 0, length = [str length];
+    NSRange range = NSMakeRange(0, length);
+    
+    while (range.location != NSNotFound)
+    {
+        range = [str rangeOfString: @"cake" options:0 range:range];
+        
+        if (range.location != NSNotFound)
+        {
+            range = NSMakeRange(range.location + range.length, length - (range.location + range.length));
+            count++; 
+        }
+    }
+    
+    return count;
+}
+
 @end
